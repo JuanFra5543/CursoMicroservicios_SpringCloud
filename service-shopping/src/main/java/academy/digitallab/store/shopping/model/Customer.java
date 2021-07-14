878caa23da4e61b472607663e8a07aea6f439f96
Juan Francisco Ruano <juan.francisco.45@hotmail.com>
2021-07-13 22:23:09 -0500

    Corrections on config-data

--- service-shopping/src/main/java/academy/digitallab/store/shopping/model/Customer.java
+++ service-shopping/src/main/java/academy/digitallab/store/shopping/model/Customer.java
@@ -1,11 +1,14 @@
 package academy.digitallab.store.shopping.model;
 
+import java.io.Serializable;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class Customer {
+public class Customer implements Serializable {
+
     private Long id;
 
     private String numberID;
