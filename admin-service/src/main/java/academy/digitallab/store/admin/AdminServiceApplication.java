e328afe1c87788e0228d91780a116fa1474a8b36
Juan Francisco Ruano <juan.francisco.45@hotmail.com>
2021-07-14 20:57:07 -0500

    Finish admin-service

--- admin-service/src/main/java/academy/digitallab/store/admin/AdminServiceApplication.java
+++ admin-service/src/main/java/academy/digitallab/store/admin/AdminServiceApplication.java
@@ -1,6 +1,6 @@
-package academy.digitallab.store.adminad;
+package academy.digitallab.store.admin;
 
-import de.condecentric.boot.admin.server.config.EnableAdminServer
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- admin-service/src/test/java/academy/digitallab/store/admin/AdminServiceApplicationTests.java
+++ admin-service/src/test/java/academy/digitallab/store/admin/AdminServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package academy.digitallab.store.adminad;
+package academy.digitallab.store.admin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
